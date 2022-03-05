4c06480f27dc1b3b9108aeddf9249a4595e94d39
Seyyed-Mahdi-Sepahbodi <seyyedmahdisepahbodi@gmail.com>
2022-03-05 07:25:53 -0500

    make main window of the game

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Pong by Seyyed Mahdi Sepahbodi")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+
+# main game loop
+while True:
+    window.update()
